5f2f018751e14e286ab8d743d5a9ed06d6a4d0b8
Emmanuel Varela <jobtestcr@outlook.com>
2020-10-21 19:53:46 -0600

    Adding JSON Response to IBM Ceo IBN Prueba Emmanuel

--- gs-spring-boot-docker/complete/src/main/java/hello/Application.java
+++ gs-spring-boot-docker/complete/src/main/java/hello/Application.java
@@ -11,10 +11,16 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello IBM World, you have two Rest API avaible this one and  ";
+		return "Hello IBM World, you have two Rest API available from the Public Internet this one (http://52.149.171.173/) and  http://52.149.171.173/ibmhistoryceos where you will fing IBM Ceo";
 	}
 
 
+	@RequestMapping("/ibmhistoryceos")
+	public String getIbmHistoryCeo() {
+		return "{\"ceo\":\"Arvind Krishna (2020–)\", \"ceo\":\"Virginia M.Rometty (2012–2020)\"},\n" +
+				"{\"ceo\":\"Samuel J. Palmisano (2002–2012)\", \"ceo\":\"Virginia M. Rometty (2012–2020)\"},\n" +
+				"{\"ceo\":\"Louis V. Gerstner, Jr. (1993–2002)\", \"ceo\":\"John F. Akers (1985–1993)\"}";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
